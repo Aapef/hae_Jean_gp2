a5829a0b10a1bc656f8e059be598d153ea190780
Jean <jean01021999@gmail.com>
2019-10-04 14:06:03 +0200

    Cours1

--- cours1/Cours1/Cours1.cpp
+++ cours1/Cours1/Cours1.cpp
@@ -4,6 +4,8 @@
 #include "pch.h"
 #include <iostream>
 
+static void TestRec();
+
 int StrChr(char * grange, char chat) {
 	int pos = -1;
 	int size = strlen(grange);
@@ -91,9 +93,66 @@ void Memcpy2(char * dest, char * src, int size) {
 }
 
 
+int Max(int a, int b) {
+	return(a < b ? a : b);
+}
+
+int Min(int a, int b) {
+	return(a > b ? a : b);
+}
+
+int StrCmp(char* meule, char* aiguille)
+{
+int lenMeule = strlen(meule);
+int lenAiguille = strlen(aiguille);
+
+int maxLen = Max(lenMeule, lenAiguille);
+int minLen = Min(lenMeule, lenAiguille);
+
+for (int i = 0; i < minLen; i++)
+{
+
+	if (meule[i] < aiguille[i]) return -1;
+	else if (meule[i] > aiguille[i])
+		return 1;
+}
+if (minLen == maxLen) return 0;
+if (maxLen == lenMeule) return 1;
+
+return -1;
+}
+
+
+
+
+char* strstr3(char* meule, char* aiguille) 
+{
+	int length = strlen(meule);
+	int length2 = strlen(aiguille);
+
+	for (int i = 0; i < length; i++) {
+		bool found = true;
+		for (int j = 0; j < length2; j++) {
+			if (meule[j] != aiguille[j]) {
+				found = false;
+				break;
+			}
+		}
+		if (found) {
+			return meule;
+		}
+			meule++;
+	}
+	return nullptr;
+}
+
+
+
+
+
 int main()
 {
-	char licorne[32] = "licorne";
+	/*char licorne[32] = "licorne";
 	char vomi[32] = "vomit";
 	Memcpy2(licorne, vomi, strlen(licorne) + 1);
 	printf("licorne %s\n", licorne);
@@ -101,4 +160,40 @@ int main()
 	char chateau[32] = "chateau";
 	char soldat = 'e';
 	printf("position : %i\n", StrChr4(chateau, soldat));
+
+	char texte[32] = "lorem ipsum dolor sit amet";
+	char token[32] = "dolor";
+
+	char * tokenInText = strstr3(texte, token);
+
+	int pos = (int)(tokenInText - texte);
+	printf("le token est en position %d\n", pos);
+	printf("length : %i\n", StrLen(texte));*/
+
+	TestRec();
+}
+
+
+
+int StrLen(char* src) {
+	if (*src == 0) return 0;
+	else return 1 + StrLen(src + 1);
+}
+
+
+int addrec(int a, int b) {
+	if (b == 0) return a;
+	else { printf("a = %i  b = %i\n", a, b); return addrec(a + 1, b - 1);  }
+}
+
+int addrec2(int a, int b) {
+	if (b == 0) return a;
+	else { printf("a = %i  b = %i\n", a, b); return 1 + addrec2(a, b - 1); }
+}
+
+void TestRec() 
+{
+	int foo = addrec(5, 6);
+	int foo2 = addrec2(6, 9);
+	int i = 0;
 }
