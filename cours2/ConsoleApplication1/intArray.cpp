672b89ae3b89def265eee6234d677c0768fe663d
Jean <jean01021999@gmail.com>
2019-10-11 11:26:15 +0200

    Cours2

--- cours2/ConsoleApplication1/intArray.cpp
+++ cours2/ConsoleApplication1/intArray.cpp
@@ -2,4 +2,4 @@
 //#include <iostream>
 #include "intArray.hpp"
 
-int intArray::TEST = 66;
+//int intArray::TEST = 66;
--- cours2/ConsoleApplication1/intArray.hpp
+++ cours2/ConsoleApplication1/intArray.hpp
@@ -1,10 +1,37 @@
 #pragma once
+#include <cstdio>
+#include <cstdlib>
+#include <string>
 
 class intArray 
 {
 
 public: 
+	std::string name;
 	int * array;
-	int maxSize;
-	int curSize;
+	int maxSize = 0;
+	int curSize = 0;
+
+	intArray(int size, const char * name = nullptr)
+	{
+		this->name = name;
+		printf("construction %s\n", this ->name.c_str());
+
+		//C old school
+		// array = (int *)malloc(size*sizeof(int)); memset(array,0,size * sizeof(int));
+
+		//C new school
+		//array = (int * )calloc(size, sizeof(int)); (calloc mets les 0)
+
+		//C++
+		array =  new int[size];
+		for(int i = 0; i < size; i++) array[i] = 0;
+
+		maxSize = size;
+	}
+
+	~intArray() 
+	{
+		printf("destruction %s\n", name.c_str());
+	}
 };
