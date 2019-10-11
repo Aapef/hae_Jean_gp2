82edf8a2e1713dbb56aceb445a21676e3a1d9ff3
Jean <jean01021999@gmail.com>
2019-10-11 14:06:27 +0200

    Cours2

--- cours2/ConsoleApplication1/ConsoleApplication1.cpp
+++ cours2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,6 +3,18 @@
 #include <iostream>
 #include "intArray.hpp"
 
+
+
+
+
+void assert(bool boolou) {
+	if (boolou == false)
+	{
+		throw new std::string("nope");
+	}
+}
+
+
 int main()
 {
 	//printf("%d", intArray::TEST)
@@ -15,4 +27,18 @@ int main()
 	{
 		intArray * tab1 = new intArray(16, nametab1); // = variable dynamique
 	}
+	intArray montableau = intArray(16, "tablo");
+	for (int i = 0; i < montableau.maxSize; i++) {
+		montableau.set(i, i);
+	}
+	montableau.set(15, 5);
+	montableau.set(16, 8);
+	montableau.pushfirst(51);
+	montableau.pushback(52);
+	montableau.insert(40, 8);
+	int i = montableau[8];
+
+	intArray montableau2 = intArray(0, "tablo2");
+	montableau2.insert(51, 0);
+
 }
--- cours2/ConsoleApplication1/intArray.hpp
+++ cours2/ConsoleApplication1/intArray.hpp
@@ -34,4 +34,77 @@ public:
 	{
 		printf("destruction %s\n", name.c_str());
 	}
+
+	void ensure(int size) 
+	{
+		if (size <= maxSize) {
+			return;
+		}
+		else {
+			int * array2 = new int[size];
+			for (int i = 0; i < size; i++) 
+			{ 
+				if (i < maxSize) array2[i] = array[i]; 
+				else array2[i] = 0; 
+			}
+			maxSize = size;
+			delete(array);
+			array = array2;
+		}
+	}
+
+	void set(int pos, int elem) 
+	{
+		ensure(pos + 1);
+		array[pos] = elem;
+		curSize = pos + 1;
+	}
+
+	void pushfirst(int elem) 
+	{
+		if (curSize >= maxSize) ensure(maxSize + 1);
+		int * array2 = new int[maxSize];
+		for (int i = 0; i < maxSize; i++)
+		{
+			array2[i] = array[i];
+		}
+
+		for (int i = 0; i < maxSize; i++) 
+		{
+			if (i > 0) { array[i] = array2[i - 1]; }
+		}
+		array[0] = elem;
+		curSize = curSize + 1;
+	}
+
+	void pushback(int elem) 
+	{
+		if (curSize >= maxSize) ensure(maxSize + 1);
+		if (curSize < maxSize) array[curSize] = elem;
+		curSize = curSize + 1;
+	}
+
+
+	void insert(int elem, int pos)
+	{
+		if(curSize >= maxSize) ensure(maxSize + 1);
+		int * array2 = new int[maxSize];
+		for (int i = 0; i < maxSize; i++)
+		{
+			array2[i] = array[i];
+		}
+
+		for (int i = pos; i < maxSize; i++)
+		{
+			if (i > pos) { array[i] = array2[i - 1]; }
+		}
+		array[pos] = elem;
+		if (curSize < maxSize) curSize = curSize + 1;
+	}
+
+	int operator[] (int i) 
+	{
+		ensure(i);
+		return array[i];
+	}
 };
